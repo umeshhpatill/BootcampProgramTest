a0542aca24878863e21d0a19bac3445ee384557d
umesh <usp5758@gmail.com>
2020-04-28 18:48:05 +0530

    Updated HelloWorld to add Good night message

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,6 @@
 public class Helloworld{
         public static void main (String[] argv){
         System.out.println("Hello World to coding club. testing");
+        System.out.println("Good night..");
 }
 }
